85c59ce6a590fd3194afe525564f85805174006a
Sijan666 <666majharulislam@gmail.com>
2026-09-13 14:08:46 +0600

    refactor: improve configuration

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,19 @@
-require("node:dns").setServers(["1.1.1.1", "8.8.8.8"]);
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const dbConnection = require("./config/mongoDBConnection");
-const authRoute = require("./routes/authRoute");
+// require('node:dns').setServers(['1.1.1.1'],['8.8.8.8'])
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoDb = require('./config/mongoDb');
+const authRoute = require('./routes/authRoute');
 
 const app = express();
 
-// middlewares
+mongoDb()
+
 app.use(express.json());
 app.use(cors());
-app.use("/api/v1/auth", authRoute);
 
-const PORT = process.env.PORT || 5000;
-dbConnection();
+app.use('/api/v1/auth', authRoute)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on: ${PORT}`);
-});
+app.listen(5000, () => {
+    console.log("server is running")
+})
