5f50d4ccc4b42b468300684867197fce3bd24a95
Majharul Islam <666majharulislam@gmail.com>
2026-09-14 22:16:37 +0600

    refactor : remove unused codes

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const {registrationController,loginController,verifyController} = require("../controllers/authControllers");
+const express = require("express")
+const router = express.Router()
+const {registrationController,loginController,verifyController} = require("../controllers/authControllers")
 
-router.post("/registration", registrationController);
-router.post("/login", loginController);
-router.get("/verify/:token", verifyController);
+router.post("/registration", registrationController)
+router.post("/login", loginController)
+router.get("/verify/:token", verifyController)
 
-module.exports = router;
+module.exports = router
