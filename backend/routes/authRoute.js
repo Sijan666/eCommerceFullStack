61bd06d57bde1a5ada7a30663c73e7601aa2acab
Sijan666 <666majharulislam@gmail.com>
2026-09-16 14:49:05 +0600

    feat : setup auth routes

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {registrationController,loginController,verifyController, forgetPasswordController} = require("../controllers/authControllers")
+const {registrationController,loginController,verifyController, forgetPasswordController, resetPasswordController} = require("../controllers/authControllers")
 
 router.post("/registration", registrationController)
 router.post("/login", loginController)
 router.post("/verify/:token", verifyController)
 router.post("/forgetpassword", forgetPasswordController)
+router.post("/resetpassword/:token", resetPasswordController)
 
 module.exports = router
