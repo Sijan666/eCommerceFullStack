6f4b7bdf121f3703b78e327bac0745c558973d4a
Sijan666 <666majharulislam@gmail.com>
2026-09-17 22:42:14 +0600

    feat : add all users controller for admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,12 @@
-const adminController =  (req,res) => {
-    res.send("this is admin controller");
+const User = require('../models/userSchema')
+
+const alluserController = async (req,res) => {
+    let users = await User.find({}).select('-password')
+    res.status(200).json({
+        success : true,
+        message : `${users.length} users found`,
+        data : users
+    })
 }
 
-module.exports = {adminController}
+module.exports = {alluserController}
