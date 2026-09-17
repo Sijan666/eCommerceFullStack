77626a7c26a16580584939c33e5a75b90b20d2a8
Sijan666 <666majharulislam@gmail.com>
2026-09-17 22:42:50 +0600

    refactor : update main router configuration

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const authRoute = require('./routes/authRoute');
 const userRoute = require('./routes/userRoute');
 const adminRoute = require('./routes/adminRoute');
 const vendorRoute = require('./routes/vendorRoute');
-const { adminMiddleware } = require('./middlewares/roleMiddleware');
+const { adminMiddleware, vendorMiddleware, userMiddleware } = require('./middlewares/roleMiddleware');
 
 
 const app = express();
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1/auth', authRoute)
-app.use('/api/v1/user', userRoute)
+app.use('/api/v1/user',userMiddleware, userRoute)
 app.use('/api/v1/admin',adminMiddleware, adminRoute)
-app.use('/api/v1/vendor', vendorRoute)
+app.use('/api/v1/vendor',vendorMiddleware, vendorRoute)
 
 const port = process.env.PORT || 5000;
 
