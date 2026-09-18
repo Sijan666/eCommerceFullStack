dcbec95b18773d91bd693ae0610abbdea4060f38
Sijan666 <666majharulislam@gmail.com>
2026-09-19 03:57:10 +0600

    feat(models): define category schema

---
+++ backend/models/categorySchema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    status: {
+        type: String,
+        enum: ['active' , 'deactive' , 'rejected'] ,
+        default: 'deactive'
+    }
+})
+
+module.exports = mongoose.model("Categories", categorySchema);
