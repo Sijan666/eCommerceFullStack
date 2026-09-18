d8a00fbda56a6533f13161c314af04218aab17f9
Sijan666 <666majharulislam@gmail.com>
2026-09-19 03:57:58 +0600

    docs(routes): add swagger annotations for user routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,66 @@
 const express = require("express")
-const { userController } = require("../controllers/userController")
+const { updateprofileController, createCategory, allCategories } = require("../controllers/userController")
 const router = express.Router()
 
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: user panel routes
+ */
 
-router.get("/products", userController)
+/**
+ * @swagger
+ * /api/v1/user/updateprofile:
+ *   post:
+ *     summary: update user profile
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fullName:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: success
+ */
+router.post("/updateprofile", updateprofileController)
 
+/**
+ * @swagger
+ * /api/v1/user/createcategory:
+ *   post:
+ *     summary: request a new category
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: success
+ */
+router.post("/createcategory", createCategory)
+
+/**
+ * @swagger
+ * /api/v1/user/allcategories:
+ *   get:
+ *     summary: get all categories
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: success
+ */
+router.get("/allcategories", allCategories)
 
 module.exports = router
