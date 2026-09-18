f9c8b124470f39f3fc1cbccaa15f7bf6981169ec
Sijan666 <666majharulislam@gmail.com>
2026-09-18 22:49:38 +0600

    feat: implement delete user controller for admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userSchema')
 
+// all users
 const alluserController = async (req,res) => {
     let users = await User.find({}).select('-password')
     res.status(200).json({
@@ -9,4 +10,32 @@ const alluserController = async (req,res) => {
     })
 }
 
-module.exports = {alluserController}
+// delete user
+const deleteUserController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        if (!deletedUser) {
+            return res.status(404).json({
+                success: false,
+                message: "user not found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "user deleted successfully"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "server error",
+            error
+        });
+    }
+};
+
+module.exports = {alluserController, deleteUserController}
